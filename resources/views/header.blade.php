91ee35fa17967fdaed3efeb013395ff685145ab3
blessingmwiti <blessingmwiti@gmail.com>
2023-02-27 11:43:50 +0300

    responsive header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">e-Comm</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -13,7 +13,7 @@
           <a class="nav-link" href="#">Orders</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Cart (0)</a>
+          <a class="nav-link" href="">Cart (0)</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
